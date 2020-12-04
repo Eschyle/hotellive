2b648f4ddaa2b85f296f9506c7e58e7bbdb300ff
Francois <francois.caillat@viacesi.fr>
2020-12-04 11:25:13 +0100

    ADD gestoin des Optionns de chambre

---
+++ src/Form/OptionType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Option;
+use App\Entity\Room;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OptionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [])
+            ->add('description', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('price', MoneyType::class, [
+                'grouping' => true,
+            ])
+            ->add('rooms', EntityType::class, [
+                'class' => Room::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Option::class,
+        ]);
+    }
+}
